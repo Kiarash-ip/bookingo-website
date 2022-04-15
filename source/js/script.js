867923b28646519ff1352fe6b5252452531e6541
Kiarash Iranpour <kia.iranpour1380@gmail.com>
2022-04-16 00:13:38 +0430

    style modified

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,6 @@ const $ = document;
 const navbarElem = $.querySelector(".navbar");
 
 window.addEventListener("scroll", (e) => {
-  console.log($.documentElement.scrollTop);
   if ($.documentElement.scrollTop > 85) {
     navbarElem.classList.add("active");
   } else {
