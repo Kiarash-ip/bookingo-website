c6749d9d88f4d5ecb54245a23b6bb69c8dcb9338
Kiarash Iranpour <kia.iranpour1380@gmail.com>
2022-04-22 18:12:01 +0430

    sections dynamic

--- source/js/script.js
+++ source/js/script.js
@@ -7,6 +7,7 @@ const controllers = $.querySelectorAll(".controller");
 const menu = $.querySelector(".navbar__list-btns-container");
 const hamburgerMenuBtn = $.querySelector(".open-menu-btn");
 const menuIcon = $.querySelector(".menu-icon");
+const sectionsContainer = $.querySelector(".website-sections-container");
 
 let scrollArr = [];
 window.addEventListener("scroll", (e) => {
@@ -87,9 +88,9 @@ prevBtn.addEventListener("click", () => {
   prevSlide();
 });
 
-// setInterval(() => {
-//   nextSlide();
-// }, 7000);
+setInterval(() => {
+  nextSlide();
+}, 7000);
 controllers.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let id = null;
@@ -124,3 +125,96 @@ hamburgerMenuBtn.addEventListener("click", () => {
     open = true;
   }
 });
+
+const bookingoDatas = [
+  {
+    id: 1,
+    title: "رقابت - یادگیری - هیجان",
+    description:
+      "خود را در رقابت با بهترین برنامه نویسان به چالش بکشید ، مهارت خود را به رخ بقیه بکشید و جایزه بگیرید یا ( جایزه بگیرید و مهارت خود را به رخ بقیه بکشید)",
+    image: "https://unsplash.it/600/400",
+  },
+  {
+    id: 2,
+    title: "فرصتی برای یادگیری و رشد حرفه ای",
+    description:
+      "با تکیه بر متخصصین ما در زمینه آموزش ، خود را به زندگی هوشنمد پیوند بزنید",
+    image: "https://unsplash.it/600/400",
+  },
+  {
+    id: 3,
+    title: "همکاری با ما",
+    description:
+      "بخشی از گروه با استعداد ما در زمینه مهندسی نرم افزار، طراحی محصول، پشتیبانی کاربر، و متخصصان فروش باشید که در هر کاری که انجام می دهیم، شور، خلاقیت و سرگرمی را با هم ترکیب می کنیم!",
+    image: "https://unsplash.it/600/400",
+  },
+  {
+    id: 4,
+    title: "طراحی رویاهای شما با تیم بوکینگو",
+    description:
+      "بوکینگ با داشتن تیمی از جوانان متخصص آماده ارائه خدمت به شما عزیزان می باشد",
+    image: "https://unsplash.it/600/400",
+  },
+];
+
+let side = null;
+let template = null;
+bookingoDatas.forEach((item) => {
+  side = item.id % 2 === 0 ? "right" : "left";
+  template =
+    side === "left"
+      ? sectionTemplateLeftGenerator(item)
+      : sectionTemplateRightGenerator(item);
+  sectionsContainer.insertAdjacentHTML("beforeend", template);
+});
+
+function sectionTemplateLeftGenerator(data) {
+  return `
+  <section class="section-leftSide">
+        <div class="section-leftSide-container">
+          <div class="section-leftSide-image-container">
+            <img
+              src="${data.image}"
+              alt="Express the text"
+            />
+          </div>
+          <div class="section-leftSide-content">
+            <h5 dir="rtl">${data.title}</h5>
+            <p dir="rtl">
+              ${data.description}
+            </p>
+            <div dir="rtl" class="section-leftSide-link-container">
+              <a href="#">مشاهده</a>
+              <i class="fa-solid fa-chevron-left"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+  `;
+}
+function sectionTemplateRightGenerator(data) {
+  return `
+  <section class="section-rightSide">
+        <div class="section-rightSide-container">
+          <div class="section-rightSide-content">
+            <h5 dir="rtl">
+              ${data.title}
+            </h5>
+            <p dir="rtl">
+              ${data.description}
+            </p>
+            <div dir="rtl" class="section-rightSide-link-container">
+              <a href="#">مشاهده</a>
+              <i class="fa-solid fa-chevron-left"></i>
+            </div>
+          </div>
+          <div class="section-rightSide-image-container">
+            <img
+              src="${data.image}"
+              alt="Express the text"
+            />
+          </div>
+        </div>
+      </section>
+  `;
+}
